a466abecb156086a5755b335477a558e8e0ca2e9
Viktor Manko <mankovictor@gmail.com>
2023-12-18 11:07:40 +0200

    13

--- src/numbers-tasks.js
+++ src/numbers-tasks.js
@@ -200,8 +200,12 @@ function roundToPowerOfTen(num, pow) {
  *   16 => false
  *   17 => true
  */
-function isPrime(/* n */) {
-  throw new Error('Not implemented');
+function isPrime(n) {
+  const nSqrt = Math.round(Math.sqrt(n));
+  for (let i = 2; i <= nSqrt; i += 1) {
+    if (n % i === 0) return false;
+  }
+  return true;
 }
 
 /**
@@ -219,8 +223,11 @@ function isPrime(/* n */) {
  *   toNumber(42, 0) => 42
  *   toNumber(new Number(42), 0) => 42
  */
-function toNumber(/* value, def */) {
-  throw new Error('Not implemented');
+function toNumber(value, def) {
+  if (Number.isNaN(+value)) {
+    return def;
+  }
+  return Number(value);
 }
 
 /**
